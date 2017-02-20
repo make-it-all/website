01bc8d4d2a1ad780bb3d0e270f1c5999b6edafda
Chris Head <chrishead90@hotmail.com>
2017-02-20 10:26:22 +0000

    stuff again

--- app/models/call.php
+++ app/models/call.php
@@ -1,19 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-/*
-  Call (Model)
-  The call model is a php represntation of the table named `calls`.
-  See \Chronicle\Base for more infomation.
-
-  @contributers Henry Morgan, Chris Head, Zach Nurcombe
-*/
-
-class Call extends ApplicationModel {
-
-  public static $table_name = 'calls';
-
-=======
 class Call extends \Chronicle\Base {
 
   public static $table_name = 'calls';
@@ -22,13 +8,12 @@ class Call extends \Chronicle\Base {
     return Problem::where(['call_id'=>$this->id])->results();
   }
 
->>>>>>> 9663bf58080a7ff52b1f12bffb55ebeabf151c02
   public function caller(){
     return Personnel::find($this->caller_id);
   }
 
   public function operator(){
-<<<<<<< HEAD
+
     return User::find($this->operator_id);
   }
 
@@ -42,14 +27,3 @@ class Call extends \Chronicle\Base {
     'description' => ['presence'=>true, ],
 
   ];
-
-=======
-   return User::find($this->operator_id);
-  }
-
-  public function updated_by(){
-   return User::find($this->updated_by);
-  }
-
->>>>>>> 9663bf58080a7ff52b1f12bffb55ebeabf151c02
-}
