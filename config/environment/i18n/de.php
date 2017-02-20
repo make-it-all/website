3aac0c44bbc65ab1ccb26a291bf6b0981fec72b5
zachnurc <zach.nurcombe@gmail.com>
2017-02-20 09:45:30 +0000

    added i18n

---
+++ config/environment/i18n/de.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+The I18n (German) file contains variables for all the the German translations of the keywords on the website,
+Internationalisation (German)
+The views pull the correct string from the relevant file depending on both browser preference
+and saved user preference.
+
+@Contributers Zach Nurcombe
+*/
+
+$header = [
+  'signed_in_message'=>'Als <b>Alice Help</b> angemeldet - ',
+  'logout'=>'Ausloggen',
+];
+
+$pagination = [
+  'showing_text'=>'Showing 1-6 of 136',
+];
+
+$titles = [
+  'dashboard'=>'Dashboard',
+  'calls'=>'Telefonieren',
+  'problems'=>'Probleme',
+  'personnel'=>'Personal',
+  'users'=>'Benutzer',
+  'reports'=>'Berichte',
+  'settings'=>'Einstellungen',
+  'hardware'=>'Hardware',
+  'software'=>'Software',
+  'title'=>'Alles Macher',
+  ];
+
+$actions = [
+  'new'=>'Neu',
+  'all'=>'Alle',
+  'edit'=>'Bearbeiten',
+  'delete'=>'Löschen',
+  'search'=>'Suche',
+];
+
+$roles = [
+  'admin'=>'Administrator',
+  'operator'=>'Operator',
+  'specialist'=>'Spezialist',
+  'lboroadmin'=>'Lboro Administrator',
+];
+
+$table_headings = [
+  'id'=>'ID',
+  'name'=>'Name',
+  'email'=>'Email',
+  'last_seen_at'=>'Zuletzt gesehen bei',
+  'role'=>'Arbeit',
+  'type'=>'Art',
+  'operator_id'=>'Operator ID',
+  'caller_id'=>'Anrufer ID',
+  'updated_by'=>'Aktualisiert von',
+  'updated_at'=>'Aktualisiert am',
+  'job_title'=>'Berufsbezeichnung',
+  'branch_id'=>'Niederlassung',
+  'department_id'=>'Abteilung',
+  'telephone_number'=>'Telefonnummer',
+  'specialization'=>'Spezialisierung',
+  'submitted_by'=>'Eingereicht von',
+  'assigned_to'=>'Zugewiesen an',
+  'solution_id'=>'Lösung',
+];
