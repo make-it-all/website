5fdee24d089acbb4945e01bc78078b91c291ff50
zachnurc <zach.nurcombe@gmail.com>
2017-02-20 11:26:55 +0000

    chinese

---
+++ config/i18n/ar.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Internationalisation (Arabic)
+The I18n (Arabic) file contains variables for all the the Arabic translations of the keywords on the website,
+The views pull the correct string from the relevant file depending on both browser preference
+and saved user preference.
+
+@Contributers Zach Nurcombe
+*/
+
+$header = [
+  'signed_in_message'=>'Signed in as <b>{{name}}</b> - ',
+  'logout'=>'Log Out',
+];
+
+$pagination = [
+  'showing_text'=>'Showing {{low}}-{{high}} of {{total}}',
+];
+
+$titles = [
+  'dashboard'=>'Dashboard',
+  'calls'=>'Calls',
+  'problems'=>'Problems',
+  'personnel'=>'Personnel',
+  'users'=>'Users',
+  'reports'=>'Reports',
+  'settings'=>'Settings',
+  'hardware'=>'Hardware ',
+  'software'=>'Software ',
+  'title'=>'Make It All',
+  ];
+
+$actions = [
+  'new'=>'New',
+  'all'=>'All',
+  'edit'=>'Edit',
+  'delete'=>'Delete',
+  'search'=>'Search',
+];
+
+$roles = [
+  'admin'=>'Admin',
+  'operator'=>'Operator',
+  'specialist'=>'Specialist',
+  'lboroadmin'=>'Lboro Admin',
+];
+
+$table_headings = [
+  'name'=>'Name',
+  'email'=>'Email',
+  'last_seen_at'=>'Last Seen At',
+  'role'=>'Role',
+  'type'=>'Type',
+  'operator_id'=>'Operator',
+  'caller_id'=>'Caller',
+  'updated_by'=>'Updated By',
+  'updated_at'=>'Updated At',
+  'job_title'=>'Job Title',
+  'branch_id'=>'Branch',
+  'department_id'=>'Department',
+  'telephone_number'=>'Phone Number',
+  'specialization'=>'Specialization',
+  'personnel_identifier'=>'Personnel Id',
+  'submitted_by'=>'Submitted By',
+  'assigned_to'=>'Assigned To',
+  'solution_id'=>'Solution',
+  'description'=>'Description',
+  'keywords'=>'Keywords',
+  'subject'=>'Subject',
+];
